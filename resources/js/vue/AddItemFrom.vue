29f3b28399fb7f1f5bd53dae053d5dab3b8eb484
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-10-18 17:35:32 +0200

    post request

--- resources/js/vue/AddItemFrom.vue
+++ resources/js/vue/AddItemFrom.vue
@@ -1,10 +1,74 @@
 <template>
     <div class="addItem">
-        <input type="text" />
-        <font-awesome-icon icon="plus-square" />
+        <input type="text" v-model="item.name" />
+        <font-awesome-icon
+            :icon="['fas', 'plus-square']"
+            @click="addItem"
+            :class="[item.name ? 'active' : 'inactive', 'plus']"
+        />
     </div>
 </template>
 
 <script>
-    export default {};
+    import axios from "axios";
+
+    export default {
+        data() {
+            return {
+                item: {
+                    name: "",
+                },
+            };
+        },
+        methods: {
+            addItem() {
+                if (this.item.name === "") {
+                    return;
+                }
+
+                axios
+                    .post("/api/item/store", {
+                        item: this.item,
+                    })
+                    .then(response => {
+                        if (response.status === 201) {
+                            this.item.name = "";
+                        }
+                    })
+                    .catch(err => {
+                        console.error("API request error:", err);
+                    });
+            },
+        },
+    };
 </script>
+
+<style scoped>
+    .addItem {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    input {
+        background-color: #f7f7f7;
+        border: none;
+        outline: none;
+        padding: 5px;
+        margin-right: 10px;
+        width: 100%;
+    }
+
+    .plus {
+        font-size: 20px;
+    }
+
+    .active {
+        color: #489c56;
+        cursor: pointer;
+    }
+
+    .inactive {
+        color: #999;
+    }
+</style>
