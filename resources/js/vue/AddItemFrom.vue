fd4836cf0500cef4293f02e5f95ae857877c31dc
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-10-18 16:45:55 +0200

    add todolist structure

---
+++ resources/js/vue/AddItemFrom.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="addItem">
+        <input type="text" />
+        <button />
+    </div>
+</template>
+
+<script>
+    export default {};
+</script>
--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -1,9 +1,39 @@
 <template>
-    <div class="todoListContainer">Hello World!</div>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-from />
+        </div>
+
+        <list-view />
+    </div>
 </template>
 
 <script>
+    import addItemFrom from "./AddItemFrom.vue";
+    import listView from "./ListView.vue";
+
     export default {
         name: "App",
+        components: {
+            addItemFrom,
+            listView,
+        },
     };
 </script>
+
+<style scoped>
+    .todoListContainer {
+        width: 350px;
+        margin: auto;
+    }
+
+    .heading {
+        background-color: #e6e6e6;
+        padding: 10px;
+    }
+
+    #title {
+        text-align: center;
+    }
+</style>
